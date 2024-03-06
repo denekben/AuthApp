89cd0251095ff41c781f9e10a1e35a2be75dd6c0
Denis Kurbanaev <denekben1@yandex.ru>
2024-03-06 13:09:01 +0300

    added roles configuration

---
+++ AuthApp/Configurations/RolesConfiguration.cs
@@ -0,0 +1,4 @@
+﻿namespace AuthApp.Configurations {
+    public class RolesConfiguration {
+    }
+}
--- AuthApp/Data/AppDbContext.cs
+++ AuthApp/Data/AppDbContext.cs
@@ -6,19 +6,6 @@ using Microsoft.EntityFrameworkCore;
 namespace AuthApp.Data {
     public class AppDbContext : IdentityDbContext<AppUser> {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        protected override void OnModelCreating(ModelBuilder modelBuilder) {
-            base.OnModelCreating(modelBuilder);
-            List<IdentityRole> roles = new List<IdentityRole> {
-                new IdentityRole {
-                    Name = "Admin",
-                    NormalizedName = "ADMIN"
-                },
-                new IdentityRole {
-                    Name = "User",
-                    NormalizedName = "USER"
-                }
-            };
-            modelBuilder.Entity<IdentityRole>().HasData(roles);
-        }
+
     }
 }
